ca72d88fa6bd0399b9099ac02c08c71169e596b2
Ronald Dilley <ronald.dilley.ctr@usgs.gov>
2018-02-05 10:12:05 -0600

    Enhancements for new environment variable.

--- deregister-job-definitions.sh
+++ deregister-job-definitions.sh
@@ -1,5 +1,4 @@
 . ../aws-environment.conf
 
 
-aws batch deregister-job-definition --job-definition ${AWS_JOB_DEFINITION_NAME_1}:1
-aws batch deregister-job-definition --job-definition ${AWS_JOB_DEFINITION_NAME_1}:2
+aws batch deregister-job-definition --job-definition ${AWS_JOB_DEFINITION_NAME_1}:$1
