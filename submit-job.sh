4cbc4ec7fb9699ac7a4e109a96ad4748d407a142
Ronald Dilley <ronald.dilley.ctr@usgs.gov>
2018-04-23 20:47:44 +0000

    Updated to submit jobs

--- submit-job.sh
+++ submit-job.sh
@@ -3,7 +3,8 @@
 mkdir -p ${AWS_CONFIGURED_DIR}
 
 cat ${AWS_TEMPLATE_DIR}/submit-job-template.json | \
-sed -e "s/<AWS_JOB_QUEUE_NAME>/${AWS_JOB_QUEUE_NAME_1}/" \
+sed -e "s/<AWS_JOB_NAME>/${1}/" \
+    -e "s/<AWS_JOB_QUEUE_NAME>/${AWS_JOB_QUEUE_NAME_1}/" \
     -e "s/<AWS_JOB_DEFINITION_NAME>/${AWS_JOB_DEFINITION_NAME_1}/" \
     > ${AWS_CONFIGURED_DIR}/submit-job-1.json
 
