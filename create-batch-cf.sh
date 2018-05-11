c57b796918101848166c127e02a3474c120f252c
Ronald Dilley <ronald.dilley.ctr@usgs.gov>
2018-05-11 19:18:08 +0000

    Example create batch stack

---
+++ create-batch-cf.sh
@@ -0,0 +1,22 @@
+. ../aws-environment.conf
+
+mkdir -p ${AWS_CONFIGURED_DIR}
+
+cat ${AWS_TEMPLATE_DIR}/batch-environment-cf-template.json | \
+sed -e "s/<AWS_STACK_NAME>/${AWS_STACK_NAME}/" \
+    -e "s/<AWS_COMPUTE_ENVIRONMENT_RESOURCE_NAME>/${AWS_COMPUTE_ENVIRONMENT_RESOURCE_NAME}/" \
+    -e "s/<AWS_JOB_QUEUE_RESOURCE_NAME>/${AWS_JOB_QUEUE_RESOURCE_NAME}/" \
+    -e "s/<AWS_JOB_DEFINITION_RESOURCE_NAME>/${AWS_JOB_DEFINITION_RESOURCE_NAME}/" \
+    -e "s/<AWS_COMPUTE_RESOURCES_MIN_VCPUS>/${AWS_COMPUTE_RESOURCES_MIN_VCPUS}/" \
+    -e "s/<AWS_COMPUTE_RESOURCES_MAX_VCPUS>/${AWS_COMPUTE_RESOURCES_MAX_VCPUS}/" \
+    -e "s/<AWS_COMPUTE_RESOURCES_DESIRED_VCPUS>/${AWS_COMPUTE_RESOURCES_DESIRED_VCPUS}/" \
+    -e "s/<AWS_INSTANCE_TYPES>/${AWS_INSTANCE_TYPES}/" \
+    -e "s/<AWS_SUBNETS>/${AWS_SUBNETS}/" \
+    -e "s/<AWS_SECURITY_GROUPS>/${AWS_SECURITY_GROUPS}/" \
+    -e "s/<AWS_INSTANCE_ROLE>/${AWS_INSTANCE_ROLE}/" \
+    -e "s/<AWS_SERVICE_ROLE>/${AWS_SERVICE_ROLE}/" \
+    > ${AWS_CONFIGURED_DIR}/batch-environment-cf.json
+
+
+aws cloudformation create-stack \
+    --cli-input-json file://${AWS_CONFIGURED_DIR}/batch-environment-cf.json
